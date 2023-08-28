fa7a95e675da0529917be9e6729faaf7482e2369
Ruy-u <119630026+Ruy-u@users.noreply.github.com>
2023-08-28 17:50:05 +0300

    Add files via upload

---
+++ Winter Plants/1.4/Source/WinterPlants/CyberFlora/WinterPlants.cs
@@ -0,0 +1,170 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace Ruyu.WinterPlants
+{
+    //Extension for xml
+    public class WinterPlantMod : DefModExtension
+    {
+    }
+    //Main winterplant class for fertility
+    public class WinterPlant : Plant
+    {
+        //changing growth rate for temperature
+        public float GrowthRateFactorFor_Temperature
+        {
+            get
+            {
+                float num, lowTemp = -50f, highTemp = 10f;
+                if (!GenTemperature.TryGetTemperatureForCell(base.Position, base.Map, out num))
+                {
+                    return 1f;
+                }
+                if (num < lowTemp)
+                {
+                    return Mathf.InverseLerp(lowTemp - 8, lowTemp, num);
+                }
+                if (num > highTemp)
+                {
+                    return Mathf.InverseLerp(highTemp + 10, highTemp, num);
+                }
+                return 1f;
+            }
+        }
+        //Override of plant growthrate to allow growth in lower/higher temps
+        public override float GrowthRate
+        {
+            get
+            {
+                if (Blighted)
+                {
+                    return 0f;
+                }
+                return GrowthRateFactor_Fertility * GrowthRateFactorFor_Temperature * GrowthRateFactor_Light * GrowthRateFactor_NoxiousHaze;
+            }
+        }
+        //Override inspection of plant fertility rate
+        public override string GetInspectString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            if (this.LifeStage == PlantLifeStage.Growing)
+            {
+                stringBuilder.AppendLine("PercentGrowth".Translate(this.GrowthPercentString));
+                stringBuilder.AppendLine("GrowthRate".Translate() + ": " + this.GrowthRate.ToStringPercent());
+                if (!this.Blighted)
+                {
+                    if (this.Resting)
+                    {
+                        stringBuilder.AppendLine("PlantResting".Translate());
+                    }
+                    if (!this.HasEnoughLightToGrow)
+                    {
+                        stringBuilder.AppendLine("PlantNeedsLightLevel".Translate() + ": " + this.def.plant.growMinGlow.ToStringPercent());
+                    }
+                    float growthRateFactor_Temperature = this.GrowthRateFactorFor_Temperature;
+                    if (growthRateFactor_Temperature < 0.99f)
+                    {
+                        if (growthRateFactor_Temperature < 0.01f)
+                        {
+                            stringBuilder.AppendLine("OutOfIdealTemperatureRangeNotGrowing".Translate());
+                        }
+                        else
+                        {
+                            stringBuilder.AppendLine("OutOfIdealTemperatureRange".Translate(Mathf.RoundToInt(growthRateFactor_Temperature * 100f).ToString()));
+                        }
+                    }
+                }
+            }
+            else if (this.LifeStage == PlantLifeStage.Mature)
+            {
+                if (this.HarvestableNow)
+                {
+                    stringBuilder.AppendLine("ReadyToHarvest".Translate());
+                }
+                else
+                {
+                    stringBuilder.AppendLine("Mature".Translate());
+                }
+            }
+            if (this.DyingBecauseExposedToLight)
+            {
+                stringBuilder.AppendLine("DyingBecauseExposedToLight".Translate());
+            }
+            if (this.Blighted)
+            {
+                stringBuilder.AppendLine("Blighted".Translate() + " (" + this.Blight.Severity.ToStringPercent() + ")");
+            }
+            return stringBuilder.ToString().TrimEndNewlines();
+        }
+        //plant leafless fix
+        public override float LeaflessTemperatureThresh => Rand.RangeSeeded(-68f, -59f, thingIDNumber ^ 0x31F3A5C1);
+        //Fixes tick issues
+        public override void TickLong()
+        {
+            CheckMakeLeafless();
+            if (base.Destroyed)
+            {
+                return;
+            }
+
+            float num = this.growthInt;
+            bool flag = this.LifeStage == PlantLifeStage.Mature;
+            this.growthInt += this.GrowthPerTick * 2000f;
+            if (this.growthInt > 1f)
+            {
+                this.growthInt = 1f;
+            }
+            if (((!flag && this.LifeStage == PlantLifeStage.Mature) || (int)(num * 10f) != (int)(this.growthInt * 10f)) && this.CurrentlyCultivated())
+            {
+                base.Map.mapDrawer.MapMeshDirty(base.Position, MapMeshFlag.Things);
+            }
+
+            if (!this.HasEnoughLightToGrow)
+            {
+                this.unlitTicks += 2000;
+            }
+            else
+            {
+                this.unlitTicks = 0;
+            }
+            this.ageInt += 2000;
+            if (this.Dying)
+            {
+                Map map = base.Map;
+                bool isCrop = this.IsCrop;
+                bool harvestableNow = this.HarvestableNow;
+                bool dyingBecauseExposedToLight = this.DyingBecauseExposedToLight;
+                int num2 = Mathf.CeilToInt(this.CurrentDyingDamagePerTick * 2000f);
+                base.TakeDamage(new DamageInfo(DamageDefOf.Rotting, (float)num2, 0f, -1f, null, null, null, DamageInfo.SourceCategory.ThingOrUnknown, null));
+                if (base.Destroyed)
+                {
+                    if (isCrop && this.def.plant.Harvestable && MessagesRepeatAvoider.MessageShowAllowed("MessagePlantDiedOfRot-" + this.def.defName, 240f))
+                    {
+                        string key;
+                        if (harvestableNow)
+                        {
+                            key = "MessagePlantDiedOfRot_LeftUnharvested";
+                        }
+                        else if (dyingBecauseExposedToLight)
+                        {
+                            key = "MessagePlantDiedOfRot_ExposedToLight";
+                        }
+                        else
+                        {
+                            key = "MessagePlantDiedOfRot";
+                        }
+                        Messages.Message(key.Translate(this.GetCustomLabelNoCount(false)).CapitalizeFirst(), new TargetInfo(base.Position, map, false), MessageTypeDefOf.NegativeEvent, true);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Winter Plants/1.4/Source/WinterPlants/CyberFlora/WinterPlantsPatch.cs
@@ -0,0 +1,130 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Ruyu.WinterPlants
+{
+    //Patch to change descriptions of winter plants
+    [HarmonyPatch(typeof(ThingDef), nameof(ThingDef.SpecialDisplayStats))]
+    public static class WinterPlantsDescPatch
+    {
+        [HarmonyPostfix]
+        public static void PlantDescPatch(ref IEnumerable<StatDrawEntry> __result, ThingDef __instance)
+        {
+            if (__instance.HasModExtension<WinterPlantMod>())
+                __result = PassthroughMethod(__result, __instance);
+        }
+        public static IEnumerable<StatDrawEntry> PassthroughMethod(IEnumerable<StatDrawEntry> __result, ThingDef __instance)
+        {
+            float minTemp = -58f;
+            float maxTemp = 20f;
+            float growMinGlow = 1f;
+            foreach (StatDrawEntry entry in __result)
+            {
+                if (entry.displayOrderWithinCategory == 4152)
+                {
+                    yield return new StatDrawEntry(StatCategoryDefOf.Basics,
+                        "MinGrowthTemperature".Translate(), minTemp.ToStringTemperature(),
+                        "Stat_Thing_Plant_MinGrowthTemperature_Desc".Translate(), 4152);
+                }
+                else if (entry.displayOrderWithinCategory == 4153)
+                {
+                    yield return new StatDrawEntry(StatCategoryDefOf.Basics,
+                        "MaxGrowthTemperature".Translate(), maxTemp.ToStringTemperature(),
+                        "Stat_Thing_Plant_MaxGrowthTemperature_Desc".Translate(), 4153);
+                }
+                else if (entry.displayOrderWithinCategory == 4154)
+                {
+                    yield return new StatDrawEntry(StatCategoryDefOf.Basics,
+                        "LightRequirement".Translate(), growMinGlow.ToStringPercent(),
+                        "Stat_Thing_Plant_LightRequirement_Desc".Translate(), 4154);
+                }
+                else
+                {
+                    yield return entry;
+                }
+            }
+        }
+    }
+    [HarmonyPatch(typeof(PlantUtility), nameof(PlantUtility.GrowthSeasonNow))]
+    public static class GrowthSeasonNowPatch
+    {
+        [HarmonyPrefix]
+        public static bool Patch_GrowthSeasonNow(ref bool __result, ref IntVec3 c, Map map, bool forSowing)
+        {
+            ThingDef thingDef = WorkGiver_Grower.CalculateWantedPlantDef(c, map);
+            if (thingDef != null && thingDef.thingClass == typeof(WinterPlant))
+            {
+                __result = GrowthSeasonNowWinterPlant(c, map); ;
+                return false;
+            }
+            return true;
+        }
+        public static bool GrowthSeasonNowWinterPlant(IntVec3 c, Map map)
+        {
+            Room roomOrAdjacent = c.GetRoomOrAdjacent(map, RegionType.ImpassableFreeAirExchange | RegionType.Normal | RegionType.Portal);
+            if (roomOrAdjacent == null)
+            {
+                return false;
+            }
+            float temperature = c.GetTemperature(map);
+            if (temperature < -58f)
+                return false;
+            else if (temperature > 20f)
+                return false;
+            else
+                return true;
+        }
+        [HarmonyPatch(typeof(Zone_Growing), nameof(Zone_Growing.GetInspectString))]
+        public static class ZoneInspectStringPatch
+        {
+            [HarmonyPrefix]
+            public static bool ZoneGetInspectStringPatch(ref Zone_Growing __instance, ref string __result)
+            {
+                ThingDef plantDefToGrow = __instance.GetPlantDefToGrow();
+                if (plantDefToGrow.thingClass != typeof(WinterPlant))
+                {
+                    return true;
+                }
+                string text = "";
+                if (!__instance.cells.NullOrEmpty())
+                {
+                    IntVec3 c = __instance.cells.First();
+                    if (c.UsesOutdoorTemperature(__instance.Map) && GrowthSeasonNowWinterPlant(c, __instance.Map))
+                    {
+                        string text2 = text;
+                        text = text2 + "(" + "ColdImmuneWinterPlants".Translate() + ")\n";
+                    }
+                    text = (__result = ((!GrowthSeasonNowWinterPlant(c, __instance.Map)) ? (text + "CannotGrowBadSeasonTemperature".Translate()) : (text + "GrowSeasonHereNow".Translate())));
+                }
+                return false;
+            }
+        }
+    }
+    [HarmonyPatch(typeof(WildPlantSpawner), nameof(WildPlantSpawner.CanRegrowAt))]
+    public static class CanRegrowAt_Patch
+    {
+        [HarmonyPostfix]
+        public static void RegrowOnCold(IntVec3 c, Map ___map, ref bool __result, ref WildPlantSpawner __instance)
+        {
+            if (___map.Biome == BiomeDefOf.Tundra || ___map.Biome == BiomeDefOf.IceSheet)
+            {
+                __result = !c.Roofed(___map);
+            }
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class WinterPlantPatch
+    {
+        static WinterPlantPatch()
+        {
+            Harmony val = new Harmony("Ruyu.WinterPlants");
+            val.PatchAll();
+        }
+    }
+}
